65f2e6c46ba8ae7dbffcd1f33cb219f8122949c8
prasannag331-alt <prasannag331@gmail.com>
2026-07-28 11:04:38 +0530

    Update decorator.py

--- decorator.py
+++ decorator.py
@@ -1,1 +1,12 @@
+def greet_message(func):
+    def wrapper():
+        print("Welcome!")
+        func()
+        print("Have a nice day!")
+    return wrapper
 
+@greet_message
+def greet():
+    print("Hello, Rahul!")
+
+greet()
