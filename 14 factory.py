d7820d4b8c0d4789a5eed3984de35a64bc2afbc2
prasannag331-alt <prasannag331@gmail.com>
2026-08-15 18:03:57 +0530

    Update 14 factory.py

--- 14 factory.py
+++ 14 factory.py
@@ -1,1 +1,27 @@
+class Dog:
+    def speak(self):
+        return "Bark"
 
+class Cat:
+    def speak(self):
+        return "Meow"
+
+class AnimalFactory:
+    @staticmethod
+    def get_animal(animal):
+        if animal.lower() == "dog":
+            return Dog()
+        elif animal.lower() == "cat":
+            return Cat()
+        else:
+            return None
+
+# Main Program
+choice = input("Enter animal (dog/cat): ")
+
+animal = AnimalFactory.get_animal(choice)
+
+if animal:
+    print("Animal says:", animal.speak())
+else:
+    print("Invalid animal!")
